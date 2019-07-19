af98c5d82ba7d928d2efe68a97ef39daf9d6ef34
Willy Camargo <camargowilly@gmail.com>
2019-07-19 15:14:46 -0300

    update: react lint

--- react.js
+++ react.js
@@ -4,6 +4,20 @@ module.exports = {
     'react/jsx-filename-extension': ['off'],
     'react/prop-types': ['warn'],
     'react/button-has-type': 0,
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/label-has-for': ['error', {
+      allowChildren: true,
+    }],
+    'jsx-a11y/anchor-is-valid': ['error', {
+      specialLink: ['to'],
+    }],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true,
+    }],
+    'import/no-unresolved': ['error', {
+      commonjs: true,
+      amd: true,
+    }],
   },
   parser: 'babel-eslint',
   plugins: [
