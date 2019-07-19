548b04ede6a1f225fb8349a2b56f60e17d67020f
Willy Camargo <camargowilly@gmail.com>
2019-07-19 13:43:50 -0300

    add: prettier config

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = {
-  extends: [ 'airbnb' ],
+  extends: [ 'airbnb', 'prettier' ],
+  rules: {
+    'semi': 0,
+    'max-len': ['error', { 'code': 100 }],
+    'prfer-promise-reject-erroers': ['off'],
+    'react/jsx-filename-extension': ['off'],
+    'react/prop-types': ['warn'],
+    'no-return-assign': ['off']
+  },
   parser: 'babel-eslint',
   plugins: [
     'react',
